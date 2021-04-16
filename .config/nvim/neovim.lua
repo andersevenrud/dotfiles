1e7f8c43e3579d9f5f060aa2a05cde7dda06c21c
Anders Evenrud <andersevenrud@gmail.com>
2021-04-17 00:36:29 +0200

    [neovim] updated lsp capabilities

--- .config/nvim/neovim.lua
+++ .config/nvim/neovim.lua
@@ -12,6 +12,13 @@ local nvim_lsp = require('lspconfig')
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.textDocument.completion.completionItem.snippetSupport = true
+capabilities.textDocument.completion.completionItem.resolveSupport = {
+    properties = {
+        'documentation';
+        'detail';
+        'additionalTextEdits';
+    }
+}
 
 nvim_lsp.dockerls.setup{
     capabilities = capabilities;
