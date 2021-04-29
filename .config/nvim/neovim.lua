1643ff65105e169cf95f3b6c93f57cf363b82a7c
Anders Evenrud <andersevenrud@gmail.com>
2021-04-30 00:11:45 +0200

    [neovim] updated blankline character

--- .config/nvim/neovim.lua
+++ .config/nvim/neovim.lua
@@ -221,7 +221,7 @@ require'bufferline'.setup{
 -------------------------------------------------------------------------------
 
 vim.g.indent_blankline_show_trailing_blankline_indent = false
-vim.g.indent_blankline_char = '┊'
+vim.g.indent_blankline_char = '│'
 vim.g.indent_blankline_buftype_exclude = { 'help', 'terminal' }
 vim.g.indent_blankline_use_treesitter = true
 vim.g.indent_blankline_show_first_indent_level = false
