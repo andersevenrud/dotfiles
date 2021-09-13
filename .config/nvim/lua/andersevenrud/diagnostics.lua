dca48d11ab97cbc6a3bce7a1da67f09ec03689d2
Anders Evenrud <andersevenrud@gmail.com>
2021-09-14 00:23:14 +0200

    [neovim] clean up diagnostics

--- .config/nvim/lua/andersevenrud/diagnostics.lua
+++ .config/nvim/lua/andersevenrud/diagnostics.lua
@@ -3,11 +3,13 @@
 -- Anders Evenrud <andersevenrud@gmail.com>
 --
 
+-- These are my custom dianostics sorces
+-- NOTE: https://github.com/jose-elias-alvarez/null-ls.nvim/pull/188
+
 local nls = require'null-ls'
 local nlsh = require'null-ls.helpers'
 local nlsm = require'null-ls.methods'
 
--- TODO: Submit this as a PR
 local stylelint = nlsh.make_builtin({
     factory = nlsh.generator_factory,
     method = nlsm.internal.DIAGNOSTICS,
@@ -44,40 +46,22 @@ local stylelint = nlsh.make_builtin({
     }
 })
 
-local null_with = function(root_file, prefix)
-    return function(cmd, builtin)
-        return nlsh.conditional(function(utils)
-            local project_local_bin = prefix .. cmd
-
-            return builtin.with({
-                command = utils.root_has_file(project_local_bin) and project_local_bin or cmd,
-                conditional = function()
-                    return utils.root_has_file(root_file)
-                end,
-            })
-        end)
-    end
-end
-
-local null_npx = null_with('package.json', 'node_modules/.bin/')
-local null_composer = null_with('composer.json', 'vendor/bin/')
-
-return function()
-    return {
-        sources = {
-            null_npx('eslint', nls.builtins.formatting.eslint),
-            null_npx('prettier', nls.builtins.formatting.prettier),
-            null_composer('php-cs-fixer', nls.builtins.formatting.phpcsfixer),
-            nls.builtins.formatting.stylua.with({
-                conditional = function(utils)
-                    return utils.root_has_file('stylua.toml')
-                end
-            }),
-
-            null_npx('eslint', nls.builtins.diagnostics.eslint),
-            null_npx('stylelint', stylelint),
-            null_composer('phpcs', nls.builtins.diagnostics.phpcs),
-            nls.builtins.diagnostics.luacheck,
-        }
+local stylelintfix = nlsh.make_builtin({
+    factory = nlsh.formatter_factory,
+    method = nlsm.internal.FORMATTING,
+    filetypes = { 'scss', 'less', 'css', 'sass' },
+    generator_opts = {
+        command = 'stylelint',
+        args = { '--fix', '--stdin', '-' },
+        to_stdin = true,
     }
-end
+})
+
+return {
+    formatting = {
+        stylelint = stylelintfix
+    },
+    diagnostics = {
+        stylelint = stylelint
+    }
+}
