7375d54713482f36053041c4470c0f28e8458508
Anders Evenrud <andersevenrud@gmail.com>
2021-11-16 22:57:29 +0100

    [neovim] source all visible buffers in cmp

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -429,7 +429,17 @@ neovim.load({
         local sources = {
             { name = 'nvim_lua' },
             { name = 'nvim_lsp' },
-            { name = 'buffer' },
+            {
+                name = 'buffer',
+                get_bufnrs = function()
+                    local bufs = {}
+                    for _, win in ipairs(vim.api.nvim_list_wins()) do
+                        bufs[vim.api.nvim_win_get_buf(win)] = true
+                    end
+                    return vim.tbl_keys(bufs)
+                    --return vim.api.nvim_list_bufs()
+                end
+            },
             { name = 'luasnip' },
             { name = 'tmux' }
         }
