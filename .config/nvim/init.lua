68e1cce16974e0e3c03fb7f8883e2d2c91088eeb
Anders Evenrud <andersevenrud@gmail.com>
2022-04-17 02:36:02 +0200

    [neovim] migrate cmp breaking changes

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -460,13 +460,13 @@ neovim.load({
                         menu = menu,
                     }),
                 },
-                mapping = {
+                mapping = cmp.mapping.preset.insert({
                     ['<C-d>'] = cmp.mapping.scroll_docs(-4),
                     ['<C-f>'] = cmp.mapping.scroll_docs(4),
                     ['<C-Space>'] = cmp.mapping.complete(),
                     ['<C-e>'] = cmp.mapping.close(),
                     ['<CR>'] = cmp.mapping.confirm({ select = true })
-                },
+                }),
             },
             cmdline = {
                 ['/'] = {
